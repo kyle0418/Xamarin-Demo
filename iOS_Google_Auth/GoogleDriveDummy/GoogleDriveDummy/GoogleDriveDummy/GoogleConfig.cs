6a62167bbfcbe05892ecfe0e0f288358921cb86e
Kyle <1396676171@qq.com>
2021-08-20 15:28:29 +0800

    Add files via upload

---
+++ iOS_Google_Auth/GoogleDriveDummy/GoogleDriveDummy/GoogleDriveDummy/GoogleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleDriveDummy
+{
+    public class GoogleConfig
+    {
+        public static string scope = "https://www.googleapis.com/auth/userinfo.email";
+        //public static string scope = "https://www.googleapis.com/auth/gmail.labels";
+
+        // IOS Test App
+        public static readonly string clientId = "225896618697-j0e8st27e2h46cj5dqf406ng0uej323c.apps.googleusercontent.com";
+        //public static readonly string redirectUrl = "net.axcrypt.axcrypt2.xamforms:/oauth2redirect";
+
+        public static readonly string redirectUrl = "com.googleusercontent.apps.225896618697-j0e8st27e2h46cj5dqf406ng0uej323c:/oauth2redirect";
+    }
+}
---
+++ iOS_Google_Auth/GoogleDriveDummy/GoogleDriveDummy/GoogleDriveDummy/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Auth.Presenters;
+using Xamarin.Forms;
+
+namespace GoogleDriveDummy
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectGoogleDrive_Clicked(object sender, EventArgs e)
+        {
+            var googleDriveInstance = GoogleDriveSingelton.GetInstance;
+            var presenter = new OAuthLoginPresenter();
+            presenter.Login(googleDriveInstance.auth);
+        }
+    }
+}
