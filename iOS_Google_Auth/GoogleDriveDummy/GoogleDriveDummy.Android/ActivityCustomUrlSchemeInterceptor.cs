5d9516017d48076c5f338ff08156f609fe611481
Kyle <1396676171@qq.com>
2021-08-20 15:43:05 +0800

    Add files via upload

---
+++ iOS_Google_Auth/GoogleDriveDummy/GoogleDriveDummy.Android/ActivityCustomUrlSchemeInterceptor.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Auth;
+
+namespace GoogleDriveDummy.Droid
+{
+    [Activity(Label = "ActivityCustomUrlSchemeInterceptor", NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+    [IntentFilter(new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "com.companyname.googledrive" }, DataPath = "/oauth2redirect")]
+    public class ActivityCustomUrlSchemeInterceptor : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            global::Android.Net.Uri uri_android = Intent.Data;
+            CustomTabsConfiguration.CustomTabsClosingMessage = null;
+            var uri = new Uri(Intent.Data.ToString());
+            AuthenticatorHelper.OAuth2Authenticator.OnPageLoading(uri);
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.SetFlags(ActivityFlags.ClearTop | ActivityFlags.SingleTop);
+            StartActivity(intent);
+            this.Finish();
+            return;
+        }
+    }
+}
---
+++ iOS_Google_Auth/GoogleDriveDummy/GoogleDriveDummy/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoogleDriveDummy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        protected override void OnAppLinkRequestReceived(Uri uri)
+        {
+            // DOES NOT FIRE EITHER
+            base.OnAppLinkRequestReceived(uri);
+        }
+    }
+}
