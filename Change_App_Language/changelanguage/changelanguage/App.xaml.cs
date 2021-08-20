7ecf54f8d7250ea6aa25062807b62603e0b65fef
Kyle <1396676171@qq.com>
2021-08-20 15:24:58 +0800

    Add files via upload

---
+++ Change_App_Language/changelanguage/changelanguage/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using changelanguage.Resource;
+using System;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace changelanguage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            LocalizationResourceManager.Current.PropertyChanged += Current_PropertyChanged;
+            LocalizationResourceManager.Current.Init(AppResource.ResourceManager);
+
+            MainPage = new MainPage();
+        }
+
+        private void Current_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            AppResource.Culture = LocalizationResourceManager.Current.CurrentCulture;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Change_App_Language/changelanguage/changelanguage/MainPageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using changelanguage.Resource;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace changelanguage
+{
+    class MainPageViewModel
+    {
+        public class LanguagePair
+        {
+            public Func<string> name;
+            public string value;
+        }
+
+        public MainPageViewModel()
+        {
+            CurrentLanguage = new LocalizedString(() => GetCurrentLanguageName());
+
+            ChangeLanguageCommand = new AsyncCommand(ChangeLanguage);
+            languageMapping = new List<LanguagePair>();
+            languageMapping.Add(new LanguagePair { name = () => AppResource.English, value = "en" });
+            languageMapping.Add(new LanguagePair { name = () => AppResource.Spanish, value = "es" });
+        }
+
+        List<LanguagePair> languageMapping { get; }
+        public LocalizedString CurrentLanguage { get; }
+        public ICommand ChangeLanguageCommand { get; }
+        private string GetCurrentLanguageName()
+        {
+            string name = languageMapping.SingleOrDefault(m => m.value == LocalizationResourceManager.Current.CurrentCulture.TwoLetterISOLanguageName).name.ToString();
+
+            return name != null ? name : LocalizationResourceManager.Current.CurrentCulture.DisplayName;
+        }
+
+        async Task ChangeLanguage()
+        {
+            string selectedName = await Application.Current.MainPage.DisplayActionSheet(
+                AppResource.ChangeLanguage,
+                null, null,
+                languageMapping.Select(m => m.name()).ToArray());
+            if (selectedName == null)
+            {
+                return;
+            }
+
+            string selectedValue = languageMapping.Single(m => m.name() == selectedName).value;
+            LocalizationResourceManager.Current.CurrentCulture = selectedValue == null ? CultureInfo.CurrentCulture : new CultureInfo(selectedValue);
+        }
+    }
+}
